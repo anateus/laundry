6b101be93701a99f689459e860a357e73cf28c49
Michael Katsevman <mike@comprehend.com>
2015-06-24 17:34:38 -0700

    First commit!

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var load = 25*60000;    // 25 mins
+var dry = 60*60000;     // 60 mins
+var buffer = 15*60000;  // 15 mins
+
+var loadtime = new Date();
+loadtime.setHours(loadtime.getHours()+2);
+$('#leaving').val(loadtime.getHours()+":"+loadtime.getMinutes());
+
+$('#leaving').on('change', function(e) {
+    var time = $(e.currentTarget).val().split(':');
+    var leaving = new Date();
+    leaving.setHours(time[0]);
+    leaving.setMinutes(time[1]);
+    var now = new Date();
+    var loads = parseInt($('#loads').val());
+    var threshold = 2*buffer+(load*loads)+(dry*loads*0.6);
+    console.log((leaving-now)/60000);
+    console.log('Thresold:',threshold/60000);
+    if ((leaving-now)<=threshold) {
+        $('#answer').text('No.');
+    } else {
+        $('#answer').text('Yes.');
+    }
+
+});
+
+$('#loads').on('change', function(e) {
+    var loads = $(e.currentTarget).val();
+    $('#loadnum').text(loads);
+});
